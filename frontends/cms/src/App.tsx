fd3d972cfae50f41bf71482e8bc92687d8edbe9a
Kelly Crowther <crowther.kelly1@gmail.com>
2020-11-23 12:51:29 -0800

    add routing

--- frontends/cms/src/App.tsx
+++ frontends/cms/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Menu, Row, Col } from "antd";
 import {
   DeploymentUnitOutlined,
   UserOutlined,
   TrophyOutlined,
   ScheduleOutlined,
+  HomeOutlined,
 } from "@ant-design/icons";
 import "./App.css";
 import { ActivitiesList } from "./components/Activities/ActivitiesList";
@@ -30,18 +32,26 @@ function App() {
           onCollapse={(isCollapsed: boolean) => setCollapsed(isCollapsed)}
         >
           <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["5"]}>
+            <Menu.Item key="5" icon={<HomeOutlined />}>
+              Home
+              <Link to="/" />
+            </Menu.Item>
             <Menu.Item key="2" icon={<ScheduleOutlined />}>
               Reservations
+              <Link to="/reservations" />
             </Menu.Item>
             <Menu.Item key="3" icon={<TrophyOutlined />}>
               Activities
+              <Link to="/activities" />
             </Menu.Item>
             <Menu.Item key="1" icon={<UserOutlined />}>
               Users
+              <Link to="/users" />
             </Menu.Item>
             <Menu.Item key="4" icon={<DeploymentUnitOutlined />}>
               Locations
+              <Link to="/locations" />
             </Menu.Item>
           </Menu>
         </Sider>
@@ -57,7 +67,14 @@ function App() {
             </Row>
           </Header>
           <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-            <ActivitiesList />
+            <Switch>
+              <Route exact path="/activities">
+                <ActivitiesList />
+              </Route>
+              <Route path="*">
+                <h2>Route Not Found</h2>
+              </Route>
+            </Switch>
           </Content>
           <Footer style={{ textAlign: "center" }}>
             Ant Design ©2018 Created by Ant UED
