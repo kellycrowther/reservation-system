ea6613143190e9064b3d680e4e860981b9e99be4
Kelly Crowther <crowther.kelly1@gmail.com>
2020-11-23 09:38:14 -0800

    WIP - added layout - sider, header, buttonf for theme switcher

--- frontends/cms/src/App.tsx
+++ frontends/cms/src/App.tsx
@@ -1,28 +1,82 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import { Layout, Menu, Row, Col, Button } from "antd";
+import {
+  BgColorsOutlined,
+  DeploymentUnitOutlined,
+  UserOutlined,
+  TrophyOutlined,
+  ScheduleOutlined,
+} from "@ant-design/icons";
 import "./App.css";
 import { ActivitiesList } from "./components/Activities/ActivitiesList";
 import { ThemeSwitcher } from "./components/ThemeSwitcher/ThemeSwitcher";
 
+const { Header, Content, Footer, Sider } = Layout;
+
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+  const [themeSwitchOpen, setThemeSwitchOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout>
+        <Sider
+          style={{
+            overflow: "auto",
+            height: "100vh",
+            position: "fixed",
+            left: 0,
+          }}
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(isCollapsed: boolean) => setCollapsed(isCollapsed)}
         >
-          Learn React
-        </a>
-      </header>
-      <ThemeSwitcher />
-      <ActivitiesList />
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+            <Menu.Item key="2" icon={<ScheduleOutlined />}>
+              Reservations
+            </Menu.Item>
+            <Menu.Item key="3" icon={<TrophyOutlined />}>
+              Activities
+            </Menu.Item>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              Users
+            </Menu.Item>
+            <Menu.Item key="4" icon={<DeploymentUnitOutlined />}>
+              Locations
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout
+          className="site-layout"
+          style={collapsed ? { marginLeft: 80 } : { marginLeft: 200 }}
+        >
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            <Row>
+              <Col offset={12} span={12} style={{ textAlign: "right" }}>
+                {/* TODO: remove hardcoded color */}
+                <Button
+                  type="link"
+                  style={{ color: "#ffffffa6", marginRight: "10px" }}
+                  icon={<BgColorsOutlined />}
+                  onClick={() => {
+                    const open = themeSwitchOpen;
+                    setThemeSwitchOpen(!open);
+                  }}
+                >
+                  <ThemeSwitcher open={themeSwitchOpen} />
+                </Button>
+              </Col>
+            </Row>
+          </Header>
+          <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+            <ActivitiesList />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 }
--- frontends/cms/src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ frontends/cms/src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -4,14 +4,17 @@ import { Select } from "antd";
 
 const { Option } = Select;
 
-export const ThemeSwitcher = () => {
+export const ThemeSwitcher = ({ open }: { open: boolean }) => {
   const { change } = useThemeSwitcher("LIGHT");
+
+  console.info("OPEN: ", open);
 
   return (
     <Select
       defaultValue="LIGHT"
       style={{ width: 120 }}
       onChange={(value) => change(value)}
+      open={open}
     >
       <Option value="LIGHT">Light</Option>
       <Option value="DARK">Dark</Option>
